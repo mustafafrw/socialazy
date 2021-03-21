9c43d05ecdb9215d551ea7cee9954b834fc6916d
mustafa <mustafa.adiguzel@std.izmirekonomi.edu.tr>
2021-03-21 23:52:24 +0300

    youtube link structure

---
+++ pages/Watch.vue
@@ -0,0 +1,17 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+    mounted(){
+        if(this.$route.query.v){
+            this.$router.push('/comment/'+this.$route.query.v)
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
